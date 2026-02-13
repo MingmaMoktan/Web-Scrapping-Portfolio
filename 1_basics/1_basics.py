4c5379f948b4c9686ce7359a14866976ce8c388b
sb13512 <sb13512@student.samk.fi>
2026-02-13 23:10:36 +0200

    Created a simple web scrapping project with the help of beautiful soup, requests

--- 1_basics/1_basics.py
+++ 1_basics/1_basics.py
@@ -1,103 +1,44 @@
-'''
-We are learning to scrap the website using python.
-We will be using the requests library to make HTTP requests and BeautifulSoup to parse the HTML content.
-First, we need to install the required libraries:
-pip install requests
-pip install beautifulsoup4
-pip install html5lib
-Now, let's start with the basics of web scraping.
-We will be scraping the website 'https://www.example.com' for demonstration purposes.
-'''
-
-# Here is the link to the documentation of BeautifulSoup:
-# https://www.crummy.com/software/BeautifulSoup/bs4/doc/
-# And this is the link to the documentation of requests:
-# https://docs.python-requests.org/en/latest/
-
 import requests
 from bs4 import BeautifulSoup
+import csv
 
-# Step 1: Make a GET request to the website
-url = 'https://dm3339.pythonanywhere.com/'
-r = requests.get(url)
+# We need url to send a request to the server and get the content of the page. 
+# We will use the requests library to do this.
+url = 'https://books.toscrape.com/'
 
-html_content = r.content
+# Saving the response of the request in a variable called response. 
+# The response variable will contain the content of the page that we requested.
+response = requests.get(url)
 
-'''
-Step 2: Parse the HTML content using BeautifulSoup
-This parsing also converts the HTML content into a BeautifulSoup object, 
-which allows us to navigate and search the HTML tree easily.
-'''
-soup = BeautifulSoup(r.text, 'html.parser')
+# Checking the status code of the response to see if the request was successful.
+print(response.status_code)  # A status code of 200 means the request was successful.
 
-# Step 3: Extract and print the title of the webpage
-title = soup.title
-print(title)
+# The content of the page is in HTML format, so we need to parse it to extract the information we need. 
+# We will use the BeautifulSoup library to do this.
+soup = BeautifulSoup(response.content, 'html.parser')
 
 
-'''
-Types of used objects:
-1. Tag: A Tag object corresponds to an HTML tag in the document. It has attributes
-    like name, attributes, and contents. For example, <a href="https://www.example.com"> is a Tag object.
-2. NavigableString: A NavigableString object represents the text within an HTML tag. For example, in <p>Hello World</p>, "Hello World" is a NavigableString object.
-3. BeautifulSoup: The BeautifulSoup object represents the entire HTML document. It provides methods to
-    navigate and search the HTML tree. For example, soup = BeautifulSoup(html_content, 'html.parser') creates a BeautifulSoup object.
-4. Comment: A Comment object represents an HTML comment in the document. For example, <!-- This is a comment --> is a Comment object.
-5. Doctype: A Doctype object represents the document type declaration in the HTML document. For example, <!DOCTYPE html> is a Doctype object.
-6. ProcessingInstruction: A ProcessingInstruction object represents a processing instruction in the HTML document. For example, <?xml version="1.0" encoding="UTF-8"?> is a ProcessingInstruction object.
-'''
+# Now we will make program to extract title of book, price of book and link to the book.
+# We will use the find_all method of BeautifulSoup to find all the elements that contain the information we need. 
+# The find_all method takes the name of the tag and the class name as arguments and returns a list of all the elements that match the criteria.
 
-'''
-Types of used methods:
-1. find(): This method is used to find the first occurrence of a tag in the HTML document. For example, soup.find('a') will return the first <a> tag in the document.
+book_title = soup.find_all('h3')
 
-2. find_all(): This method is used to find all occurrences of a tag in the HTML
-document. For example, soup.find_all('a') will return a list of all <a> tags in the document.
+book_price = soup.find_all('p', class_='price_color')
 
-3. get_text(): This method is used to extract the text from a tag. For example
-soup.find('p').get_text() will return the text inside the first <p> tag in the document.
+book_links = soup.find_all('a', href=True)
 
-4. attrs: This attribute is used to access the attributes of a tag. For example,soup.find('a').attrs will return a dictionary of attributes for the first <a> tag in the document.
+complete_data = []
+for titles, price, links in zip(book_title, book_price, book_links):
+    book_title = titles.text
+    book_price = price.text
+    book_link = links['href']
+    complete_data.append([book_title, book_price, book_link])
+    
 
-5. name: This attribute is used to access the name of a tag. For example, soup.find('a').name will return 'a' for the first <a> tag in the document.
+# Now we will save the extracted information in a CSV file in simple format.
 
-6. contents: This attribute is used to access the contents of a tag. For example, soup.find('p').contents will return a list of the contents inside the first <p> tag in the document.
-
-7. parent: This attribute is used to access the parent tag of a tag. For example, soup.find('p').parent will return the parent tag of the first <p> tag in the document.
-
-8. next_sibling: This attribute is used to access the next sibling tag of a tag. For example, soup.find('p').next_sibling will return the next sibling tag of the first <p> tag in the document.
-
-9. previous_sibling: This attribute is used to access the previous sibling tag of a tag. For example, soup.find('p').previous_sibling will return the previous sibling tag of the first <p> tag in the document.
-
-10. select(): This method is used to select tags using CSS selectors. For example, soup.select('a') will return a list of all <a> tags in the document.
-'''
-
-# Now lets practice all the methods and attributes mentioned above on the website 'https://www.example.com' and see how they work.
-
-# 1. find()
-first_a_tag = soup.find('a')
-# print(first_a_tag)
-
-# 2. find_all() and also use pretty print to make it more readable
-all_a_tags = soup.find_all('a')
-# print(all_a_tags)
-
-# 3. get_text()
-first_p_tag_text = soup.find('p').get_text()
-# print(first_p_tag_text)
-
-# 4. attrs
-first_a_tag_attrs = soup.find('a').attrs
-# print(first_a_tag_attrs)
-
-# 5. name
-first_a_tag_name = soup.find('a').name
-# print(first_a_tag_name)
-
-# 6. contents
-first_p_tag_contents = soup.find('p').contents
-# print(first_p_tag_contents)
-
-# 7. parent
-first_p_tag_parent = soup.find('p').parent
-print(first_p_tag_parent)
+with open('books.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Title', 'Price', 'Link'])
+    writer.writerows(complete_data)
